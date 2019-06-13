2aaae3d46d7ecc7938ee19d0c493d06f53b35fb0
Brittany Rausch <berausch247@gmail.com>
2019-06-13 14:11:57 -0700

    Make a quick change to the Header file.

--- header.php
+++ header.php
@@ -4,11 +4,7 @@
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <!--Import Google Icon Font-->
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-     <!--Import materialize.css-->
-     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-     <!--Let browser know website is optimized for mobile-->
-     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
    </head>
   </head>
   <body>
